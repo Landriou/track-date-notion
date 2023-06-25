4e854bf02eaeaf3dd465287cd67196e9c71ca289
Alexander Silva <alexessilva2000@gmail.com>
2023-06-25 01:16:23 -0300

    add status

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
+const { Client } = require("@notionhq/client");
+const dotenv = require("dotenv");
 
-const { Client } = require("@notionhq/client")
-const dotenv = require("dotenv")
+dotenv.config();
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
-dotenv.config()
-const notion = new Client({ auth: process.env.NOTION_KEY })
-
-const databaseId = process.env.NOTION_DATABASE_ID
-
+const databaseId = process.env.NOTION_DATABASE_ID;
 
 startProcess().then(() => {
-  setInterval(RepeatProcess, 10000)
-})
+  setInterval(RepeatProcess, 100000);
+});
 
 /**
  * Get and set the initial data store with tasks currently in the database.
  */
 async function startProcess() {
-  const currentTasks = await getTasksAndUpdateFromNotionDatabase()
+  const currentTasks = await getTasksAndUpdateFromNotionDatabase();
 }
 
 async function RepeatProcess() {
   // Get the tasks currently in the database.
-  console.log("\nFetching tasks from Notion DB...")
-  const currentTasks = await getTasksAndUpdateFromNotionDatabase()
-  console.log(currentTasks)
+  console.log("\nFetching tasks from Notion DB...");
+  const currentTasks = await getTasksAndUpdateFromNotionDatabase();
+  console.log(currentTasks);
 }
 
 /**
@@ -31,74 +29,209 @@ async function RepeatProcess() {
  *
  */
 async function getTasksAndUpdateFromNotionDatabase() {
-  const pages = []
-  let cursor = undefined
+  const pages = [];
+  let cursor = undefined;
 
   while (true) {
     const { results, next_cursor } = await notion.databases.query({
       database_id: databaseId,
       start_cursor: cursor,
-    })
-    pages.push(...results)
+      filter: {
+        or: [
+          {
+            and: [
+              {
+                property: "Status",
+                status: {
+                  equals: "Produccion",
+                },
+              },
+              {
+                property: "DateFinished",
+                date: {
+                  is_empty: true,
+                },
+              },
+            ],
+          },
+          {
+            and: [
+              {
+                property: "Status",
+                status: {
+                  equals: "In progress Dev",
+                },
+              },
+              {
+                property: "DateStarted",
+                date: {
+                  is_empty: true,
+                },
+              },
+            ],
+          },
+          {
+            and: [
+              {
+                property: "Status",
+                status: {
+                  equals: "Done Design",
+                },
+              },
+              {
+                property: "DateFinished",
+                date: {
+                  is_empty: true,
+                },
+              },
+            ],
+          },
+          {
+            and: [
+              {
+                property: "Status",
+                status: {
+                  equals: "Empatizacion / Research UX",
+                },
+              },
+              {
+                property: "DateStarted",
+                date: {
+                  is_empty: true,
+                },
+              },
+            ],
+          },
+          {
+            and: [
+              {
+                property: "Status",
+                status: {
+                  equals: "Condensador de flujos / Definir",
+                },
+              },
+              {
+                property: "DateStarted",
+                date: {
+                  is_empty: true,
+                },
+              },
+            ],
+          },
+          {
+            and: [
+              {
+                property: "Status",
+                status: {
+                  equals: "Diseñar",
+                },
+              },
+              {
+                property: "DateStarted",
+                date: {
+                  is_empty: true,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    });
+    pages.push(...results);
     if (!next_cursor) {
-      break
+      break;
     }
-    cursor = next_cursor
+    cursor = next_cursor;
   }
-  console.log(`${pages.length} pages successfully fetched.`)
+  console.log(`${pages.length} pages successfully fetched.`);
 
-  const tasks = []
+  const tasks = [];
   for (const page of pages) {
-    const pageId = page.id
+    const pageId = page.id;
 
-    const statusPropertyId = page.properties["Status"].id
+    const statusPropertyId = page.properties["Status"].id;
     const statusPropertyItem = await getPropertyValue({
       pageId,
       propertyId: statusPropertyId,
-    })
+    });
     const status = statusPropertyItem.status.name
       ? statusPropertyItem.status.name
-      : "No Status"
+      : "No Status";
 
-    const dateFinishedPropertyId = page.properties["DateFinished"].id
+    const dateFinishedPropertyId = page.properties["DateFinished"].id;
     const dateFinishedPropertyItem = await getPropertyValue({
-          pageId,
-          propertyId: dateFinishedPropertyId,
-        })
-    const dateFinished = dateFinishedPropertyItem.date 
+      pageId,
+      propertyId: dateFinishedPropertyId,
+    });
+    const dateFinished = dateFinishedPropertyItem.date;
 
-    const titlePropertyId = page.properties["Name"].id
+    const dateStartedPropertyId = page.properties["DateStarted"].id;
+    const dateStartedPropertyItem = await getPropertyValue({
+      pageId,
+      propertyId: dateFinishedPropertyId,
+    });
+    const dateStarted = dateFinishedPropertyItem.date;
+
+    const titlePropertyId = page.properties["Name"].id;
     const titlePropertyItems = await getPropertyValue({
       pageId,
       propertyId: titlePropertyId,
-    })
+    });
     const title = titlePropertyItems
-      .map(propertyItem => propertyItem.title.plain_text)
-      .join("")
+      .map((propertyItem) => propertyItem.title.plain_text)
+      .join("");
 
+    if (dateFinished == null) {
+      if (status == "Produccion" || status == "Done Design") {
+        var tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
+        var localISOTime = new Date(Date.now() - tzoffset)
+          .toISOString()
+          .slice(0, -1);
 
-    if(dateFinished == null){
-      if(status == "Done"){
-        var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-        var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
-        
         await notion.pages.update({
           page_id: pageId,
           properties: {
-            "DateFinished": {
+            DateFinished: {
               date: {
-                start: localISOTime.split('T')[0],
+                start: localISOTime.split("T")[0],
               },
             },
           },
-        })
+        });
 
-        tasks.push({ pageId, status, title })
+        tasks.push({ pageId, status, title });
+      }
+    }
+
+    if (dateFinished == null) {
+      if (
+        status == "In progress Dev" ||
+        status == "Empatizacion / Research UX" ||
+        status == "Condensador de flujos / Definir" ||
+        status == "Diseñar"
+      ) {
+        var tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
+        var localISOTime = new Date(Date.now() - tzoffset)
+          .toISOString()
+          .slice(0, -1);
+
+        await notion.pages.update({
+          page_id: pageId,
+          properties: {
+            DateStarted: {
+              date: {
+                start: localISOTime.split("T")[0],
+              },
+            },
+          },
+        });
+
+        tasks.push({ pageId, status, title });
       }
     }
   }
 
-   return tasks
+  return tasks;
 }
 
 /**
@@ -113,25 +246,25 @@ async function getPropertyValue({ pageId, propertyId }) {
   const propertyItem = await notion.pages.properties.retrieve({
     page_id: pageId,
     property_id: propertyId,
-  })
+  });
   if (propertyItem.object === "property_item") {
-    return propertyItem
+    return propertyItem;
   }
 
   // Property is paginated.
-  let nextCursor = propertyItem.next_cursor
-  const results = propertyItem.results
+  let nextCursor = propertyItem.next_cursor;
+  const results = propertyItem.results;
 
   while (nextCursor !== null) {
     const propertyItem = await notion.pages.properties.retrieve({
       page_id: pageId,
       property_id: propertyId,
       start_cursor: nextCursor,
-    })
+    });
 
-    nextCursor = propertyItem.next_cursor
-    results.push(...propertyItem.results)
+    nextCursor = propertyItem.next_cursor;
+    results.push(...propertyItem.results);
   }
 
-  return results
+  return results;
 }
